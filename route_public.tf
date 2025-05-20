c0a2c1552269800bfae227663da24861cedd1c74
ahmadtahirkamboh <ahmadtahir4235@gmail.com>
2025-05-20 03:36:33 -0700

    Update route_public.tf

--- route_public.tf
+++ route_public.tf
@@ -11,3 +11,17 @@ tags = {
 Name = "Public Route Table" 
 } 
 } 
+################################## 
+### Route table association ## 
+################################# 
+resource "aws_route_table_association" 
+"public-subnet-1-route-table-association" { 
+subnet_id = aws_subnet.public-web-subnet-1.id 
+route_table_id = aws_route_table.public-route-table.id 
+} 
+resource "aws_route_table_association" 
+"public-subnet-2-route-table-association" { 
+subnet_id = aws_subnet.public-web-subnet-2.id 
+route_table_id = aws_route_table.public-route-table.id 
+}
+
